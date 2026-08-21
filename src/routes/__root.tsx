361a9b6060584645c04fb1e710e8669ea51b7277
antonthomas8248 <antonthomas4u@gmail.com>
2026-08-21 16:26:09 +0530

    feat: implement root route configuration and define custom CSS design system

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,7 +11,6 @@ import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
-import { Splash } from "../components/graby/splash";
 
 function NotFoundComponent() {
   return (
@@ -134,7 +133,6 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Splash />
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
     </QueryClientProvider>
